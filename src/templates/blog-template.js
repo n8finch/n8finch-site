40b7077c2f066ffc9a28b00288c74ae2b635f880
Nate Finch <finchproservices@gmail.com>
2020-04-11 12:20:40 -0500

    update the image property on SEO"

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 
 const SecondPage = ({data}) => (
   <Layout>
-    <SEO title={data.wpgraphql.post.title} description={data.wpgraphql.post.excerpt} image={data.wpgraphql.post.featuredImage && data.wpgraphql.post.featuredImage.imageFile.childImageSharp.fluid.src}/>
+    <SEO title={data.wpgraphql.post.title} description={data.wpgraphql.post.excerpt} image={data.wpgraphql.post.featuredImage && `https://n8finch.com${data.wpgraphql.post.featuredImage.imageFile.childImageSharp.fluid.src}`}/>
 
     <div className="blog-template">
       {data.wpgraphql.post.featuredImage && (
